91f37dfd5edcbed93362d7dbba75964ab0b377ff
Skander MABROUK <mabroukskander@gmail.com>
2013-08-03 20:19:08 +0200

    Add Entities

---
+++ Entity/Address.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Skonsoft\PostalAddressBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="ss_address__address")
+ * @ORM\Entity(repositoryClass="Skonsoft\PostalAddressBundle\Entity\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="complement", type="string", length=255, nullable=true)
+     */
+    private $complement;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="postal_code", type="integer")
+     */
+    private $postalCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var State
+     *
+     * @ORM\ManyToOne(targetEntity="Skonsoft\PostalAddressBundle\Entity\State")
+     * @ORM\JoinColumn(name="state_id", referencedColumnName="id", nullable=false)
+     */
+    private $state;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     *
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set complement
+     *
+     * @param string $complement
+     *
+     * @return Address
+     */
+    public function setComplement($complement)
+    {
+        $this->complement = $complement;
+
+        return $this;
+    }
+
+    /**
+     * Get complement
+     *
+     * @return string
+     */
+    public function getComplement()
+    {
+        return $this->complement;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param integer $postalCode
+     *
+     * @return Address
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return integer
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Address
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     *
+     * @return \Skonsoft\PostalAddressBundle\Entity\State
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param \Skonsoft\PostalAddressBundle\Entity\State $state
+     *
+     * @return \Skonsoft\PostalAddressBundle\Entity\Address
+     */
+    public function setState(State $state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
---
+++ Entity/Country.php
@@ -0,0 +1,157 @@
+<?php
+namespace Skonsoft\PostalAddressBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Country
+ *
+ * @ORM\Table(name="ss_address__country")
+ * @ORM\Entity(repositoryClass="Skonsoft\PostalAddressBundle\Entity\CountryRepository")
+ */
+class Country
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="iso_name", type="string", length=3, nullable=true)
+     */
+    private $isoName;
+
+    /**
+     * @var array
+     *
+     * @ORM\OneToMany(targetEntity="Skonsoft\PostalAddressBundle\Entity\State", mappedBy="country", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $states;
+
+    public function __construct()
+    {
+        $this->states = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Country
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set isoName
+     *
+     * @param string $isoName
+     *
+     * @return Country
+     */
+    public function setIsoName($isoName)
+    {
+        $this->isoName = $isoName;
+
+        return $this;
+    }
+
+    /**
+     * Get isoName
+     *
+     * @return string
+     */
+    public function getIsoName()
+    {
+        return $this->isoName;
+    }
+
+    /**
+     * Set states
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection\ArrayCollection $states
+     *
+     * @return \Skonsoft\PostalAddressBundle\Entity\Country
+     */
+    public function setStates(ArrayCollection $states)
+    {
+        $this->states = $states;
+
+        return $this;
+    }
+
+    /**
+     * Get states
+     *
+     * @return ArrayCollection
+     */
+    public function getStates()
+    {
+        return $this->states;
+    }
+
+    /**
+     * @param State $state
+     *
+     * @return \Skonsoft\PostalAddressBundle\Entity\Country
+     */
+    public function addState(State $state)
+    {
+        $state->setCountry($this);
+        $this->states->add($state);
+
+        return $this;
+    }
+
+    /**
+     * @param State $state
+     *
+     * @return \Skonsoft\PostalAddressBundle\Entity\Country
+     */
+    public function removeState(State $state)
+    {
+        $this->states->removeElement($state);
+        unset($state);
+
+        return $this;
+    }
+}
